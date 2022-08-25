ee2cb131a6b0be361fbd81d872640a49e132f26e
Krista Gartland <Randomflyingobject@yahoo.com>
2022-08-25 17:22:05 -0500

    Thymeleaf stuff

--- src/main/java/org/launchcode/hellospring/controllers/helloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/helloController.java
@@ -1,13 +1,16 @@
 package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class helloController {
      // put /hello to show the message
-    @GetMapping("hello")
+    @GetMapping()
     @ResponseBody
     public String hello() {
         return "Hello, Spring";
@@ -17,6 +20,37 @@ public class helloController {
     @ResponseBody
     public String goodbye() {
         return "Goodbye, Spring";
+    }
+
+    // handler request of the form /hello?name=LaunchCode
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST }, value = "hello")
+    public String helloWithQueryParam(@RequestParam String name, Model model) {
+        String greeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", greeting);
+        return "hello";
+
+    }
+
+    //Handles requests of the form /hello/LaunchCode
+    @GetMapping("hello/{name}")
+    public String helloWithPathParam(@PathVariable String name, Model model){
+        model.addAttribute("greeting", "Hello, " + name + "!");
+        return "hello";
+    }
+
+    @GetMapping("form")
+    public String helloForm(){
+        return "form";
+    }
+
+    @GetMapping("hello-names")
+    public String helloNames(Model model){
+        List<String> names = new ArrayList<>();
+        names.add("LaunchCode");
+        names.add("Krista");
+        names.add("Java");
+        model.addAttribute("names", names);
+        return "lists";
     }
 
 }
